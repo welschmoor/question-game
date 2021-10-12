152674081770955b76d6a37ba6ff88911946424d
Johannes Welsch <welschmoor@gmail.com>
2021-10-12 23:40:44 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import QuestionSection from "./QuestionSection.js"
+
+export default function App() {
+  return (
+    <div className="maindiv">
+      
+      <h1 className="h1heading">QUIZ</h1>
+      <QuestionSection />
+    </div>
+  );
+}
---
+++ src/QuestionSection.js
@@ -0,0 +1,16 @@
+
+import React from "react"
+
+
+
+const QuestionSection = () => {
+    
+    return(
+        <section className="question-section">
+            <button className="btn-qs">Answer1</button>
+            <button className="btn-qs">Answer2</button>
+        </section>
+    )
+}
+
+export default QuestionSection
